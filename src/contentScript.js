d1b71a710a1c9e36b2c127478458075c4a644a54
Fill4 <filipe4r@gmail.com>
2017-05-04 18:49:15 +0100

    Runs automatically on correct pages and loads all images.

--- src/contentScript.js
+++ src/contentScript.js
@@ -20,17 +20,44 @@ function getListImages(currentURL, callback) {
 	});
 }
 
+function changeImage(listImages) {
+	//get page element into variable
+	var pageTemplate = $('.page')[0].cloneNode(true);
+	//remove page element
+	pageParent = $('.page')[0].parentElement;
+	$('.page')[0].remove();
+	pageParent.setAttribute("style", "background-color: #161616;");
+	for (var page = 1; page < listImages.length+1; ++page) {
+		// create new page elements from default and change id and src of img
+		pageImage = pageTemplate.cloneNode(true);
+		pageImage.querySelector('#manga-page').src = listImages[page-1];
+		pageImage.querySelector('#manga-page').id = 'manga-page' + page;
+		// append page title
+		var header = document.createElement('h3');
+		pageParent.appendChild(header);
+		// append each new page to parent element
+		pageParent.append(pageImage);
+	}
+}
+
 // Listens for messages from the extension scripts. If message is {action: getImages} it runs
 // getListImages and then returns the list of images listImages to the extension with a response
 // {output: listImages}. Otherwise return the response {output: Wrong Action}.
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.action == "getImages") {
 		getListImages(window.location.href, function(listImages) {
-			sendResponse({output: listImages });	
+			//changeImage(listImages);
+			sendResponse({output: listImages });
 		});
 		// Used to keep the messaging channel open until a response is sent
 		return true;
 	} else{
 		sendResponse({output: "Wrong Action"});
 	}
+});
+
+$(document).ready(function() {
+	getListImages(window.location.href, function(listImages) {
+		changeImage(listImages);
+	});
 });
--- src/getImage.js
+++ src/getImage.js
@@ -5,8 +5,8 @@ function readFullChapter() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {action: 'getImages'}, function(response) {
 			try{
-				setStatus(response.output[4]);
-				setImage(response.output[4]);
+				setStatus('Displaying complete chapter');
+				//setImage(response.output[4]);
 			} catch(err) {
 				setStatus(err.message);
 			}
