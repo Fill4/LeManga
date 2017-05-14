2b84f5f838c650de39b43fe4beff00ce8a64e21e
Fill4 <filipe4r@gmail.com>
2017-05-14 20:34:55 +0100

    eventScript and checkPage now with working communication

--- src/checkPage.js
+++ src/checkPage.js
@@ -1,11 +1,9 @@
+// Runs on all pages and sends a message to eventScript to check if the url matches a desired pattern
 (function() {
 	"use strict";
 	chrome.runtime.sendMessage({action: "matchURL", url: window.location.href }, function(response) {
-		if(response.isTrue) {
-			chrome.runtime.executeScript(null, {file: response.mirrorScript}, function() {
-				console.log('Here');
-				printThis();
-			});
+		if(!response.isTrue) {
+			console.log('Page matches no patterns');
 		};
 	});
 })();
--- src/eventScript.js
+++ src/eventScript.js
@@ -1,16 +1,23 @@
+// Checks whether the url sent from checkPage matches a known pattern.
+// If so, returns the javascript file with that website implementation
 function checkURL(url, callback){
 	var isTrue = url.match(/(mangastream|readms).(com|net)/g) !== null;
-	if (isTrue) {
-		callback(isTrue, 'src/MangaStream.js');
-	};
+	callback(isTrue, 'src/MangaStream.js');
 }
 
+// Receives url from checkPage and if checkURL returns true it injects the js file 
+// from that mirror into the current page
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.action == "matchURL") {
 		checkURL(request.url, function(isTrue, mirrorScript) {
-			sendResponse({isTrue: isTrue, mirrorScript: mirrorScript});
+			if (isTrue) {
+				chrome.tabs.executeScript(sender.tab.id, {file: mirrorScript});
+				sendResponse({isTrue: isTrue, mirrorScript: mirrorScript});
+			} else {
+				sendResponse({isTrue: isTrue, mirrorScript: null});
+			};
 		});
 		// Used to keep the messaging channel open until a response is sent
 		return true;
-	}
+	};
 });
