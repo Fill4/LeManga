f3217d342868c6a7cab2cf3339c6b22f742b1f1a
Fill4 <filipe4r@gmail.com>
2017-08-31 16:25:53 +0100

    Simple test to add items to list

--- src/popup.js
+++ src/popup.js
@@ -4,10 +4,15 @@ $(function () {
     // Get version from manifest and update html
     $("#version").text(chrome.runtime.getManifest().version);
 
-    var db = new PouchDB('ListMangas');
+    var db = new PouchDB('MangaList');
     db.info().then(function (result) {
         if(result.doc_count === 0) {
             console.log('Its empty!');
         }
     });
+
+    $("#window").html("<ul id='mangaList'><li>Item 1</li></ul>");
+    setTimeout(function() {
+        $("#mangaList").append("<li>Item 2</li>");
+    }, 2000);
 });
