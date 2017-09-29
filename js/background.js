06e54f3c113ffa0de06832b21be5ed1702112a57
Fill4 <filipe4r@gmail.com>
2017-09-29 15:22:22 +0100

    Renaiming files

---
+++ js/background.js
@@ -0,0 +1,41 @@
+// Checks whether the url sent from checkPage matches a known pattern.
+// If so, returns the javascript file with that website implementation
+function checkUrl(url, callback) {
+	//var isTrue = url.match(/(mangastream|readms).(com|net)/g) !== null;
+	var mirrorMatch = url.match(/readms.net/g) !== null;
+	mirrorName = "MangaStream";
+	callback(mirrorMatch, mirrorName, "mirrors/MangaStream.js");
+}
+
+function initDBs() {
+	var db = new PouchDB('MangaList');
+}
+
+// Receives url from checkPage and if checkUrl returns true it injects the js file 
+// from that mirror into the current page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.action == "matchURL") {
+		//console.log("Checking Url: " + request.url);
+		checkUrl(request.url, function(mirrorMatch, mirrorName, mirrorScript) {
+			if (mirrorMatch) {
+				//console.log("Url: " + request.url + " matched known pattern");
+				promises = []
+				promises.push(chrome.tabs.executeScript(sender.tab.id, {file: mirrorScript}));
+				promises.push(chrome.tabs.executeScript(sender.tab.id, {file: "css/semantic/semantic.css"}));
+				promises.push(chrome.tabs.executeScript(sender.tab.id, {file: "css/semantic/semantic.js"}));
+				Promise.all(promises)
+					.then(function() {sendResponse({mirrorMatch: true, mirrorName: mirrorName});})
+					.catch(function(e) {console.log("Catch: " + e);});
+			} else {
+				sendResponse({mirrorMatch: false, mirrorName: null});
+			};
+		});
+	};
+	// Used to keep the messaging channel open until a response is sent. Async response
+	return true;
+});
+
+// Add all the code here for extension initialization
+// Setup options variables and default values
+// Setup database initialization and alarm updates with user defined intervals
+console.log("Initializing extension!");
--- js/checkPage.js
+++ js/checkPage.js
@@ -5,9 +5,6 @@ chrome.runtime.sendMessage({action: "matchURL", url: window.location.href }, fun
 	if(response.mirrorMatch) {
 		$(document).ready(function() {
 			var mirror = window[response.mirrorName];
-			if (typeof(mirror) === "undefined") {
-				setTimeout(function() {var mirror = window[response.mirrorName];}, 200);
-			}
 			mirror.getListImages(window.location.href, function(listImages) {
 				mirror.changeImages(listImages);
 			});
